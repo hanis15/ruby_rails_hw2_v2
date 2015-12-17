493bd24239ab69533b5b0a56d859c8e0037f0d15
Jaroslav Hanak <HanakJarek@gmail.com>
2015-12-17 18:04:29 +0100

    Edit mail address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'ruby_home_work2@hanak.com'
+  config.mailer_sender = 'hanakjarek@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
